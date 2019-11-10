ebee4865a80907278ae6e684657dff1ad5a0541c
kagematya <kagematya@gmail.com>
2019-11-10 16:18:58 +0900

    SRT行列用の便利クラスを追加

---
+++ SSBPLib/ss/SS5MatrixHolder.cpp
@@ -0,0 +1,103 @@
+﻿#include "SS5MatrixHolder.h"
+
+namespace ss {
+
+
+SS5MatrixHolder::SS5MatrixHolder()
+	: m_matrix()
+	, m_dirtyFlag(false)
+	, m_position(0.0f, 0.0f, 0.0f)
+	, m_rotation(0.0f, 0.0f, 0.0f)
+	, m_scale(1.0f, 1.0f, 1.0f)
+	, m_flipLR(false)
+	, m_flipTB(false)
+{
+}
+
+// SRT行列を取得
+const Matrix& SS5MatrixHolder::getMatrix()
+{
+	if (m_dirtyFlag) {
+		m_matrix = makeMatrix();
+		m_dirtyFlag = false;
+	}
+	return m_matrix;
+}
+	
+// 位置
+void SS5MatrixHolder::setPosition(float x, float y, float z) {
+	m_position.x = x;
+	m_position.y = y;
+	m_position.z = z;
+
+	m_dirtyFlag = true;
+}
+
+const Vector3& SS5MatrixHolder::getPosition() const {
+	return m_position;
+}
+
+// 回転(deg)
+void SS5MatrixHolder::setRotation(float x, float y, float z) {
+	m_rotation.x = x;
+	m_rotation.y = y;
+	m_rotation.z = z;
+
+	m_dirtyFlag = true;
+}
+
+const Vector3& SS5MatrixHolder::getRotation() const {
+	return m_rotation;
+}
+
+// スケール
+void SS5MatrixHolder::setScale(float x, float y, float z) {
+	m_scale.x = x;
+	m_scale.y = y;
+	m_scale.z = z;
+
+	m_dirtyFlag = true;
+}
+
+const Vector3& SS5MatrixHolder::getScale() const {
+	return m_scale;
+}
+
+// 左右反転
+void SS5MatrixHolder::setFlipLR(bool flipLR) {
+	m_flipLR = flipLR;
+	m_dirtyFlag = true;
+}
+
+bool SS5MatrixHolder::getFlipLR() const {
+	return m_flipLR;
+}
+
+// 上下反転
+void SS5MatrixHolder::setFlipTB(bool flipTB) {
+	m_flipTB = flipTB;
+	m_dirtyFlag = true;
+}
+
+bool SS5MatrixHolder::getFlipTB() const {
+	return m_flipTB;
+}
+
+
+// 行列を作る
+Matrix SS5MatrixHolder::makeMatrix() const {
+	Matrix mat;
+	Vector3 scale = m_scale;
+	if (m_flipLR) {
+		scale.x = -scale.x;
+	}
+	if (m_flipTB) {
+		scale.y = -scale.y;
+	}
+	Vector3 rotRadian = SSDegToRad(m_rotation);
+	mat.setupSRzyxT(scale, rotRadian, m_position);
+	return mat;
+}
+
+
+} // namespace ss
---
+++ SSBPLib/ss/SS5MatrixHolder.h
@@ -0,0 +1,49 @@
+﻿#pragma once
+#include "math/Vector3.h"
+#include "math/Matrix.h"
+
+namespace ss {
+
+/** SRT行列を持ってる人。dirtyflagで必要な時だけ計算します */
+class SS5MatrixHolder {
+public:
+	SS5MatrixHolder();
+
+	// SRT行列を取得
+	const Matrix& getMatrix();
+	
+	// 位置
+	void setPosition(float x, float y, float z = 0.0f);
+	const Vector3& getPosition() const;
+
+	// 回転(deg)
+	void setRotation(float x, float y, float z);
+	const Vector3& getRotation() const;
+
+	// スケール
+	void setScale(float x, float y, float z = 1.0f);
+	const Vector3& getScale() const;
+
+	// 左右反転
+	void setFlipLR(bool flipLR);
+	bool getFlipLR() const;
+
+	// 上下反転
+	void setFlipTB(bool flipTB);
+	bool getFlipTB() const;
+
+private:
+	Matrix  m_matrix;		// SRT行列
+	bool    m_dirtyFlag;	// 変更があったか？
+
+	Vector3 m_position;		// 位置
+	Vector3 m_rotation;		// 回転(deg)
+	Vector3 m_scale;		// スケール
+	bool	m_flipLR;		// 左右反転
+	bool	m_flipTB;		// 上下反転
+
+	// 行列を作る
+	Matrix makeMatrix() const;
+};
+
+} // namespace ss
